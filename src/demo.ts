1f6a35539ff36b3b3984ee84379bf68e043d3d5a
karolina <karolinavasko@gmail.com>
2025-03-17 15:19:34 +0300

    initial commit

---
+++ src/demo.ts
@@ -0,0 +1,23 @@
+type Item = {
+    id: number,
+    name: string,
+    author: string,
+    pages?: number
+};
+
+let item: Item = {
+    id: 1008,
+    name: 'Meteora',
+    author: 'Linkin Park'
+};
+
+item = {
+    id: 1007,
+    name: 'War and Piece',
+    author: 'Leo Tolstoy',
+    pages: 1000
+};
+
+function addToCard(item): void {
+    //TODO:
+}
---
+++ src/ts/app.ts
@@ -0,0 +1,11 @@
+import Cart from './service/cart';
+import Book from './domain/book';
+import MusicAlbum from './domain/musicalbum';
+
+const cart = new Cart();
+console.log(cart.items);
+
+cart.add(new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225));
+cart.add(new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900));
+
+console.log(cart.items);
